d62a27e3a66fc09c12d00e3ba20a536b05133c01
CiarJ <cjones.de117@gmail.com>
2024-01-28 20:19:31 +0100

    Added login logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import './LoginPage.jsx';
-import LoginPage from './LoginPage.jsx';
+import Login from './LoginPage.jsx';
+import Dashboard from './Dashboard.jsx';
+import useToken from './useToken';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { token, setToken } = useToken();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
 
   return (
     <>
       <div>
-        <LoginPage />
+        {/* <LoginPage /> */}
+
+        <h1>App</h1>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   )
--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-const LoginPage = () => {
+async function loginUser(credentials) {
+    return fetch('http://localhost:8080/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    }).then(data => data.json())
+};
+
+const Login = ( {setToken} ) => {
+
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+          username,
+          password
+        });
+        console.log(token);
+        setToken(token);
+      }
 
     return (
         <>
             <h1>Login Page</h1>
 
-            <form className="loginForm">
+            <form className="loginForm" onSubmit={handleSubmit}>
                 <label>
-                    Email:
-                    <input type="email" name="email" />
+                    Username:
+                    <input type="username" name="username" onChange={e => setUserName(e.target.value)} />
                 </label>
                 <label>
                     Password:
-                    <input type="password" name="password" />
+                    <input type="password" name="password" onChange={e => setPassword(e.target.value)} />
                 </label>
                 <input type="submit" value="Submit" />
             </form>
         </>
     );
-
 };
 
-export default LoginPage;
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+};
+
+export default Login;
